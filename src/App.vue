d4be84d8339f3c37c14141656c56256d04bf844b
Alireza Sadeghi <alirezawdx@gmail.com>
2024-04-11 18:35:55 +0330

    feat: add background image

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,15 @@
 </script>
 
 <template>
+  <div class="h-screen bg-cover rainy flex">
+    <!-- Navbar -->
 
+    <!-- Body -->
+  </div>
 </template>
+
+<style>
+.rainy {
+  background-image: url('./src/assets/img/rainy-sky.jpg');
+}
+</style>
