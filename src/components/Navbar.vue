2ce0ba2310466c247a414e81274cc9a90e4debe2
Alireza Sadeghi <alirezawdx@gmail.com>
2024-04-13 12:01:54 +0330

    refactor: add anchor to the website address in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-between items-center">
     <!-- Title -->
-    <h4 class="text-white font-bold">weather.com</h4>
+    <h4><a href="#" class="text-white hover:text-blue-500 duration-150 font-bold">weather.com</a></h4>
   </div>
 </template>
