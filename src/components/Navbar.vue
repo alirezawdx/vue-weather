cee0158ead4db80f3fa6de642c6b4a0b9acbb513
Alireza Sadeghi <alirezawdx@gmail.com>
2024-04-13 12:16:16 +0330

    feat: add developer github

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,5 +2,7 @@
   <div class="flex justify-between items-center">
     <!-- Title -->
     <h4><a href="#" class="text-white hover:text-blue-500 duration-150 font-bold">weather.com</a></h4>
+    <!-- Developer -->
+    <h5><a href="https://github.com/alirezawdx/" class="text-white font-extralight tracking-wider opacity-60">dev</a></h5>
   </div>
 </template>
