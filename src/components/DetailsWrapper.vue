ef1230acee3c8e4f550af413ba8b0ede85ede1c0
Alireza Sadeghi <alirezawdx@gmail.com>
2024-04-16 23:24:17 +0330

    feat: replace lorem ipsum with real content

--- src/components/DetailsWrapper.vue
+++ src/components/DetailsWrapper.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="">
-    <div class="text-white bg-black p-8 rounded-2xl bg-opacity-20 backdrop-blur-sm text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, aliquam eligendi ut incidunt excepturi sapiente nostrum exercitationem eaque dolorum omnis modi, sint eum corporis. Quasi delectus pariatur veritatis accusamus optio.</div>
+    <div class="text-white bg-black p-8 rounded-2xl bg-opacity-20 backdrop-blur-sm text-sm">
+      Thunderstorms expected around 00:00
+      <hr class="mt-3 opacity-50">
+    </div>
     <div class="text-white bg-black p-8 rounded-2xl bg-opacity-20 backdrop-blur-sm text-sm mt-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, aliquam eligendi ut incidunt excepturi sapiente nostrum exercitationem eaque dolorum omnis modi, sint eum corporis. Quasi delectus pariatur veritatis accusamus optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae saepe laborum sit veritatis aspernatur rerum dolor recusandae debitis nihil minus, deserunt, in, provident iure odio ab optio sed! Nisi, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolores, ratione ut exercitationem ullam optio incidunt fuga eveniet reprehenderit id, ipsum architecto et provident, dolorem repellendus aut non labore eius?</div>
   </div>
 </template>
