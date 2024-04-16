786776989327d92985a06db82732ba3457d50813
Alireza Sadeghi <alirezawdx@gmail.com>
2024-04-16 23:52:20 +0330

    feat: add vertical detais component

---
+++ src/components/VerticalData.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['temp', 'icon', 'time',])
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-2 pt-3">
+    <span class="font-medium">{{ props.time }}</span>
+    <i class="text-4xl" :class="props.icon"></i>
+    <span class="text-lg">{{ props.temp }}<span class="text-sm align-top">°C</span></span>
+  </div>
+</template>
