54d01747feb12a78e216f6af1845157812780ca5
Alireza Sadeghi <alirezawdx@gmail.com>
2024-04-16 23:12:13 +0330

    feat: add container for details

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,15 +1,16 @@
 <script setup>
 import Status from './Status.vue'
+import DetailsWrapper from './DetailsWrapper.vue'
 </script>
 
 <template>
-  <div class="flex justify-between items-end flex-col lg:flex-row text-white mt-10">
+  <div class="flex justify-between items-start flex-col lg:flex-row text-white mt-10">
     <!-- Container -->
-    <div class="p-3 basis-1/2">
+    <div class="p-3 basis-1/2 self-end">
       <Status />
     </div>
     <div class="p-3 basis-1/2">
-      <!-- Placeholder -->
+      <DetailsWrapper />
     </div>
   </div>
 </template>
